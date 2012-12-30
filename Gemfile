7a1a769b382e4716f0bd9bb58cc0bfcb428fd774
Wayne Conrad <wconrad@yagni.com>
2012-12-30 10:58:43 -0700

    Use simplecov to find code uncovered by tests

--- Gemfile
+++ Gemfile
@@ -6,4 +6,5 @@ gem 'memoizer', '~> 1.0'
 group :development do
   gem 'rspec', '~> 2.12'
   gem 'jeweler', '~> 1.8'
+  gem 'simplecov', '~> 0.7', :platform => :ruby_19
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,6 @@
+if RUBY_VERSION >= '1.9'
+  require 'simplecov'
+  SimpleCov.start
+end
+
 require File.expand_path('../lib/USPS-intelligent-barcode', File.dirname(__FILE__))
